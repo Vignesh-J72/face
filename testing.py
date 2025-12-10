d8cb6b8cb5b6109ccbc255297e434cc48f480f2e
Vignesh <vigneshj722@gmail.com>
2025-12-10 15:40:00 +0530

    Removed detect and replaced with detect1.py

--- testing.py
+++ testing.py
@@ -1,19 +1,7 @@
-import os
-import firebase_admin
-from firebase_admin import db,credentials
-import datetime
+import requests
 
-cred=credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred,{
-'databaseURL':'https://first-project-c1f7b-default-rtdb.asia-southeast1.firebasedatabase.app/'})
-
-path="Vasanth"
-name="Vasanth"
-ref=db.reference('Users/'+path.lower())
-print(ref.get())
-cur_date=datetime.datetime.now().strftime("%d-%m-%Y")
-print(db.reference('Attendance/'+path.lower()).get())
-clock_in=db.reference('Attendance/'+name.lower()+'/'+cur_date+"/Clocked in at").get()
-           
-clock_out=db.reference('Attendance/'+name.lower()+'/'+cur_date+"/Clocked out at").get()
-print(clock_out)
+#low level rest api
+url='https://first-project-c1f7b-default-rtdb.asia-southeast1.firebasedatabase.app/Attendance.json?auth=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'
+ans=requests.get(url).json()
+for i in ans:
+    print(i,ans[i])
